869555328b719de4f812f19b5c0a50ae174c54ed
Johann-28 <johann.velazquezantn@uanl.edu.mx>
2023-04-24 22:25:05 -0600

    Tarea 7

--- Tarea 7/ejecutable.py
+++ Tarea 7/ejecutable.py
@@ -1,16 +1,59 @@
 import minimax as mm
+import time
 
 board = [
-    ['X','X','O'],
-    [None,'O',None],
+    [None,None,None],
+    [None,None,None],
     [None,None,None]
 ]
 
-depth = 5
+depth = 1000
 player = 'X'
 
-best_value = mm.minimax(board, depth, player)
-best_action = mm.get_best_action(board, depth, player)
+while not mm.terminal_test(board)[0]:
+    print("Es el turno del jugador", player)
+    print("Tablero actual:")
+    mm.print_board(board)
+    
+    row = int(input("Ingresa el número de fila (0, 1, 2): "))
+    while not 0 <= row <= 2:
+        row = int(input("Rango incorrecto. Ingresa de nuevo el número de fila (0, 1, 2): "))
+       
+       
+    col = int(input("Ingresa el número de columna (0, 1, 2): "))
+    while not 0 <= col <= 2:
+        col = int(input("Rango incorrecto. Ingresa de nuevo el número de columna (0, 1, 2): "))
 
-print(f"El mejor valor para el jugador {player} es: {best_value}")
-print(f"La mejor acción para el jugador {player} es: {best_action}")
+   
+     
+    if board[row][col] is not None:
+        print("Esa casilla ya está ocupada. Intenta de nuevo.")
+        continue
+    board[row][col] = player
+    mm.print_board(board)
+    if mm.terminal_test(board)[0]:
+        break
+    
+    print("Pensando.", end="")
+    time.sleep(1)
+    print(".", end="")
+    time.sleep(1)
+    print(".", end="")
+    time.sleep(1)
+    
+    
+    inicio = time.time()
+    best_action = mm.get_best_action(board, depth, 'O')
+    board = mm.result(board, best_action)
+    fin = time.time()
+    
+    print("La máquina ha jugado:")
+    print("Tiempo destinado" , fin - inicio)
+    mm.print_board(board)
+    player = 'X' if player == 'O' else 'X'
+
+print("Juego terminado.")
+if mm.terminal_test(board)[1] is None:
+    print("¡Empate!")
+else:
+    print("Ha ganado el jugador", mm.terminal_test(board)[1])
